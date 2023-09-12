a33e3d2af6c6b965a728b98e448b850b3d17f754
eordona <eordona@umd.edu>
2023-09-11 21:21:21 -0400

    client setup

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <h1 className="text-blue-500 text-3xl underline">Hello World</h1>
+      <p className="flex justify-center">Test</p>
+    </>
+  )
+}
+
+export default App
